67adca6fc103d3d4e824a3600c0c9e460228ce9f
Michael Ennen <(none)>
2016-10-21 14:40:38 -0700

    Initial commit

---
+++ content/live-templates.rst
@@ -0,0 +1,12 @@
+IntelliJ Live Templates
+#######################
+
+:date: 2016-04-24 14:11
+:modified: 2016-04-24 14:11
+:tags: java, intellij
+:category: java
+:slug: live-templates
+:authors: Michael Ennen
+:summary: Using IntelliJ live templates to reduce monotony.
+
+This is my first, awesome, blog post.
---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Michael Ennen'
+SITENAME = u"Brcolow's Blog"
+SITEURL = 'http://brcolow.com'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Phoenix'
+
+DEFAULT_LANG = u'en'
+
+THEME = "C:\\Users\\brcolow\\pelican-themes\\svbhack"
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
